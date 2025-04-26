87c782673bd35ef2814753ed6e13ff9d56b89b91
Sakshi Priya <priyasakshi8989@gmail.com>
2025-04-26 23:19:11 +0530

    modulus operator

---
+++ modulusOperator.js
@@ -0,0 +1,8 @@
+//Write a programe to find the remainder using modulus operator
+
+let a = 29;
+let b = 15;
+
+//Apply modulus operator to find the remainder
+let remainder = a % b;
+console.log("Remainder of the given numbers:", remainder);
